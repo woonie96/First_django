d0b852a602b9bc88b8b3f7a4d401c34b7f798c8f
woonie96 <woonstar@seoultech.ac.kr>
2023-06-18 21:31:06 +0900

    init

---
+++ main/notes/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404,redirect
+from django.utils import timezone
+from .models import Question
+from .forms import QuestionForm
+# Create your views here.
+
+def index(request):
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list':question_list}
+    return render(request, 'notes/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'notes/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('notes:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('notes:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'notes/question_form.html', context)
